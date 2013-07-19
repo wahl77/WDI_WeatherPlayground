071d8fbe3972bbe2cecf7f689cdb056b853930b1
Franky W. (franklinwahl - Frankys-MacBook-Pro.local) <wahl77+git@gmail.com>
2013-07-19 09:28:47 -0700

    More stuff

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -13,7 +13,6 @@ class FileUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    binding.pry
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
@@ -40,7 +39,7 @@ class FileUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
-    %w(.csv)
+    %w(csv)
   end
 
   # Override the filename of the uploaded files:
